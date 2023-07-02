bc3e10d5e1602226437fa86d2e93e23b9b26c945
Rachelle Yeo <rachelle.yeo@outlook.com>
2023-07-02 11:16:24 +0800

    create: inventory list component

---
+++ components/InventoryList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in inventory" :key="item.id">
+        {{ item.name }} / {{ item.quantity }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InventoryList",
+  props: {
+    inventory: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
