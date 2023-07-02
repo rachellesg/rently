17ed04db23f60c9b5d2bd9dfc773d9ad990f3640
Rachelle Yeo <rachelle.yeo@outlook.com>
2023-07-02 13:11:30 +0800

    feat: add modal

---
+++ components/common/Modal.vue
@@ -0,0 +1,31 @@
+<template>
+  <dialog :id="modalId" class="modal" :open="visible">
+    <form method="dialog" class="modal-box">
+      <slot />
+    </form>
+    <form method="dialog" class="modal-backdrop">
+      <button @click="closeModal">Close</button>
+    </form>
+  </dialog>
+</template>
+
+<script>
+export default {
+  name: "ModalDialog",
+  props: {
+    modalId: {
+      type: String,
+      required: true,
+    },
+    visible: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
