6340435c2907555641a992f5704e9dfccf226fe6
Rachelle Yeo <rachelle.yeo@outlook.com>
2023-07-02 10:15:48 +0800

    feat: add selected apartment

--- components/ApartmentList.vue
+++ components/ApartmentList.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <ul>
-      <li v-for="apartment in apartments" :key="apartment.id">
+      <li
+        v-for="apartment in apartments"
+        :key="apartment.id"
+        @click="selectApartment(apartment)"
+        :class="{ active: selectedApartment === apartment }">
         <strong>Address:</strong> {{ apartment.address }}<br />
         <strong>Floor:</strong> {{ apartment.floor }}<br />
         <strong>Door Number:</strong> {{ apartment.doorNumber }}
@@ -18,6 +22,15 @@ export default {
       type: Array,
       required: true,
     },
+    selectedApartment: {
+      type: Object,
+      default: () => null,
+    },
+  },
+  methods: {
+    selectApartment(apartment) {
+      this.$emit("selectApartment", apartment);
+    },
   },
 };
 </script>
@@ -30,5 +43,8 @@ h2 {
 li {
   margin-bottom: 20px;
   list-style-type: none;
+}
+.active {
+  border: 1px solid white;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="container mx-auto">
-    <h1 class="left">{{ pageTitle }}</h1>
-    <template v-if="isLoading">
-      <span class="loading loading-dots loading-lg"></span>
-    </template>
-    <template v-else>
-      <ApartmentList :apartments="apartments" />
-      <template v-if="error">
-        <div class="error-message">{{ error }}</div>
+  <div class="container mx-auto flex justify-between">
+    <div class="w-2/5">
+      <template v-if="isLoading">
+        <span class="loading loading-dots loading-lg"></span>
       </template>
-    </template>
+      <template v-else>
+        <ApartmentList
+          :apartments="apartments"
+          :selectedApartment="selectedApartment"
+          @selectApartment="selectApartment" />
+        <template v-if="error">
+          <div class="error-message">{{ error }}</div>
+        </template>
+      </template>
+    </div>
+    <section class="w-3/5">
+      <template v-if="selectedApartment">
+        <h2>{{ selectedApartment.address }}</h2>
+      </template>
+    </section>
   </div>
 </template>
 
@@ -24,21 +33,28 @@ export default {
   },
   data() {
     return {
-      pageTitle: "Apartment List",
       isLoading: true,
       apartments: [],
+      inventory: [],
       error: null,
+      selectedApartment: null,
     };
   },
   async mounted() {
     try {
       this.apartments = await getApartmentsData();
+      this.selectedApartment = this.apartments[0] || null;
       this.isLoading = false;
     } catch (error) {
       console.error("Error fetching data:", error);
       this.isLoading = false;
       this.error = "Failed to fetch data. Please try again later.";
     }
+  },
+  methods: {
+    selectApartment(apartment) {
+      this.selectedApartment = apartment;
+    },
   },
 };
 </script>
